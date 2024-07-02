31a47ff9dd0a9e3f0b8db65bc6d15fc753330f2f
Jobert Sá <bearinmind@jobear.dev>
2024-07-02 14:08:08 +0200

    ✅ Implemented Two Sum solution and fixed tests

--- Sources/LeetSwiftCore/Solutions/001 - Two Sum/TwoSumSolution.swift
+++ Sources/LeetSwiftCore/Solutions/001 - Two Sum/TwoSumSolution.swift
@@ -9,6 +9,15 @@ import Foundation
 
 class TwoSumSolution: TwoSumDefinition {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dictionary: [Int: Int] = [:]
+        
+        for (i, num) in nums.enumerated() {
+            if let item = dictionary[target - num] {
+                return [item, i]
+            }
+            dictionary[num] = i
+        }
+        
         return []
     }
 }
