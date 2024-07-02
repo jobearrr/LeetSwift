0fdd3b56dd7265e995db1dffdd16a3d57391a34c
Jobert Sá <bearinmind@jobear.dev>
2024-07-03 00:10:37 +0200

    🚀 Added Add Two Numbers problem

---
+++ Sources/LeetSwiftCore/Problems/002 - Add Two Numbers/AddTwoNumbersDefinition.swift
@@ -0,0 +1,12 @@
+//
+//  AddTwoNumbersDefinition.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 02/07/2024.
+//
+
+import Foundation
+
+protocol AddTwoNumbersDefinition {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode?
+}
