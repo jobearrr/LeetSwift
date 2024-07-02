d950096b7ceb92b8e54650e9fbd5367137a5b585
Jobert Sá <bearinmind@jobear.dev>
2024-07-03 00:10:08 +0200

    🚀 Added ListNode to Core

---
+++ Sources/LeetSwiftCore/Core/ListNode.swift
@@ -0,0 +1,32 @@
+//
+//  ListNode.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 02/07/2024.
+//
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode? = nil
+    
+    public init() {
+        val = 0
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+    }
+    
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
+
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        lhs.val == rhs.val && lhs.next == rhs.next
+    }
+}
