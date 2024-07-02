bdda4212c107d9ebcab4646d280ef406893e99c6
Jobert Sá <bearinmind@jobear.dev>
2024-07-02 13:54:04 +0200

    🚀 Two Sum problem definition

---
+++ Sources/LeetSwiftCore/Problems/001 - Two Sum/TwoSumDefinition.swift
@@ -0,0 +1,12 @@
+//
+//  TwoSumDefinition.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 02/07/2024.
+//
+
+import Foundation
+
+protocol TwoSumDefinition {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int]
+}
