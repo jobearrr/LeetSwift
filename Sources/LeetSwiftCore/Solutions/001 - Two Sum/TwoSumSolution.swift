063b210982086305d214174d6ced2c413424bb14
Jobert Sá <bearinmind@jobear.dev>
2024-07-02 14:07:25 +0200

    🧪 Added failing tests for Two Sum

---
+++ Sources/LeetSwiftCore/Solutions/001 - Two Sum/TwoSumSolution.swift
@@ -0,0 +1,14 @@
+//
+//  TwoSumSolution.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 02/07/2024.
+//
+
+import Foundation
+
+class TwoSumSolution: TwoSumDefinition {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        return []
+    }
+}
---
+++ Tests/LeetSwiftCoreTests/Problems/001 - Two Sum/TwoSumTests.swift
@@ -0,0 +1,48 @@
+//
+//  TwoSumTests.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 02/07/2024.
+//
+
+import XCTest
+@testable import LeetSwiftCore
+
+final class TwoSumTests: XCTestCase {
+    
+    let solution: TwoSumDefinition = TwoSumSolution()
+    
+    func testSolution() {
+        for testData in data {
+            let input = testData.input
+            
+            let output = solution.twoSum(input.nums, input.target)
+            
+            XCTAssertEqual(output, testData.expectedOutput)
+        }
+    }
+}
+
+extension TwoSumTests: TestCaseProviding {
+    func validateInput(_ input: (nums: [Int], target: Int)) -> Bool {
+        input.nums.count >= 2 &&
+        input.nums.count <= 10_000 &&
+        input.target >= -1_000_000_000 &&
+        input.target <= 1_000_000_000
+    }
+    
+    var data: [TestData<(nums: [Int], target: Int), [Int]>] { [
+        TestData(
+            input: ([2, 7, 11, 15], 9),
+            expectedOutput: [0, 1]
+        ),
+        TestData(
+            input: ([3, 2, 4], 6),
+            expectedOutput: [1, 2]
+        ),
+        TestData(
+            input: ([3, 3], 6),
+            expectedOutput: [0, 1]
+        )
+    ] }
+}
