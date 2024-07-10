9ff7321317709311ee29b7c30db8d534742532f9
Jobert <jobert@codespark.io>
2024-07-10 08:23:43 +0200

    ✨ Longest Substring Without Repeating Characters
    Problem definition

---
+++ Sources/Problems/003 - Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharactersDefinition.swift
@@ -0,0 +1,13 @@
+//
+//  LongestSubstringWithoutRepeatingCharactersDefinition.swift
+//  LeetSwift
+//
+//  Created by Jobert on 10/07/2024.
+//
+
+import Foundation
+import Core
+
+public protocol LongestSubstringWithoutRepeatingCharactersDefinition {
+    func lengthOfLongestSubstring(_ s: String) -> Int
+}
