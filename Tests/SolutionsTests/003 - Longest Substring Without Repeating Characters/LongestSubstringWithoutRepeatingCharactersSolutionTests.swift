a2714b8114c185580e424ada294924478b61f01c
Jobert <jobert@codespark.io>
2024-07-10 08:37:19 +0200

    ✨ Failing solution test for problem 003

---
+++ Tests/SolutionsTests/003 - Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharactersSolutionTests.swift
@@ -0,0 +1,28 @@
+//
+//  LongestSubstringWithoutRepeatingCharactersSolutionTests.swift
+//  LeetSwift
+//
+//  Created by Jobert on 10/07/2024.
+//
+
+import XCTest
+@testable import Problems
+@testable import Solutions
+@testable import TestSupport
+
+final class LongestSubstringWithoutRepeatingCharactersSolutionTests: XCTestCase {
+    
+    let solution: LongestSubstringWithoutRepeatingCharactersDefinition = LongestSubstringWithoutRepeatingCharactersSolution()
+    
+    func testSolution() {
+        for testData in data {
+            let input = testData.input
+            
+            let output = solution.lengthOfLongestSubstring(input)
+            
+            XCTAssertEqual(output, testData.expectedOutput)
+        }
+    }
+}
+
+extension LongestSubstringWithoutRepeatingCharactersSolutionTests: LongestSubstringWithoutRepeatingCharactersTestCaseProvider { }
