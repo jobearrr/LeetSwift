7ed52c28e75c86b56e11ef5292626988ff9d130a
Jobert <jobert@codespark.io>
2024-07-12 22:16:48 +0200

    ✨ Problem 005 definition

---
+++ Sources/Problems/005 - Longest Palindromic Substring/LongestPalindromicSubstringDefinition.swift
@@ -0,0 +1,13 @@
+//
+//  LongestPalindromicSubstringDefinition.swift
+//  LeetSwift
+//
+//  Created by Jobert on 12/07/2024.
+//
+
+import Foundation
+import Core
+
+public protocol LongestPalindromicSubstringDefinition {
+    func longestPalindrome(_ s: String) -> String
+}
