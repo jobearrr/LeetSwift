a96fe37586a213b91fb159e8929ee804ea08b65d
Jobert Sá <bearinmind@jobear.dev>
2024-07-14 21:33:47 +0200

    ✨ Problem 006 definition

---
+++ Sources/Problems/006 - Zigzag Conversion/ZigzagConversionDefinition.swift
@@ -0,0 +1,13 @@
+//
+//  ZigzagConversionDefinition.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 14/07/2024.
+//
+
+import Foundation
+import Core
+
+public protocol ZigzagConversionDefinition {
+    func convert(_ s: String, _ numRows: Int) -> String
+}
