27efebe3859cc248d84b461615eb092feb75ffed
Jobert Sá <bearinmind@jobear.dev>
2024-07-15 22:55:57 +0200

    Problem 007 Definition

---
+++ Sources/Problems/007 - Reverse Integer/ReverseIntegerDefinition.swift
@@ -0,0 +1,13 @@
+//
+//  ReverseIntegerDefinition.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 15/07/2024.
+//
+
+import Foundation
+import Core
+
+public protocol ReverseIntegerDefinition {
+    func reverse(_ x: Int) -> Int
+}
---
+++ Tests/SolutionsTests/007 - Reverse Integer/ReverseIntegerTestCaseProvider.swift
@@ -0,0 +1,34 @@
+//
+//  ReverseIntegerTestCaseProvider.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 15/07/2024.
+//
+
+@testable import Core
+@testable import TestSupport
+
+protocol ReverseIntegerTestCaseProvider: TestCaseProviding { }
+
+extension ReverseIntegerTestCaseProvider {
+
+    func validateInput(_ input: Int) -> Bool {
+        // TODO: Implement validation
+        true
+    }
+
+    var data: [TestData<Int, Int>] { [
+        TestData(
+            input: 123,
+            expectedOutput: 321
+        ),
+        TestData(
+            input: -123,
+            expectedOutput: -321
+        ),
+        TestData(
+            input: 120,
+            expectedOutput: 21
+        )
+    ] }
+}
