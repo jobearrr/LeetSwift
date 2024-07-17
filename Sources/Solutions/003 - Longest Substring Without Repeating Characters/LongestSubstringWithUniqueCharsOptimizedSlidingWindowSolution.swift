22e4fd6ec947682fb239b73f01be275b99f7dd02
Jobert Sá <bearinmind@jobear.dev>
2024-07-17 18:07:14 +0200

    ✨ Problem 003 Optimized Solution

---
+++ Sources/Solutions/003 - Longest Substring Without Repeating Characters/LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolution.swift
@@ -0,0 +1,35 @@
+//
+//  LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolution.swift
+//  LeetSwift
+//
+//  Created by Jobert on 10/07/2024.
+//
+
+import Core
+import Foundation
+import Problems
+
+final class LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolution: LongestSubstringWithUniqueCharsDefinition {
+
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var indexArray = Array(repeating: -1, count: 128)
+        var maxLength = 0
+        var start = 0
+        
+        let chars = Array(s)
+
+        for i in 0..<chars.count {
+            guard let asciiValue = chars[i].asciiValue else { continue }
+            let asciiIndex = Int(asciiValue)
+            
+            if indexArray[asciiIndex] != -1 {
+                start = max(start, indexArray[asciiIndex] + 1)
+            }
+            
+            indexArray[asciiIndex] = i
+            maxLength = max(maxLength, i - start + 1)
+        }
+
+        return maxLength
+    }
+}
---
+++ Tests/SolutionsTests/003 - Longest Substring Without Repeating Characters/LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolutionTests.swift
@@ -0,0 +1,28 @@
+//
+//  LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolutionTests.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 17/07/2024.
+//
+
+import XCTest
+@testable import Problems
+@testable import Solutions
+@testable import TestSupport
+
+final class LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolutionTests: XCTestCase {
+
+    let solution: LongestSubstringWithUniqueCharsDefinition = LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolution()
+
+    func testSolution() {
+        for testData in data {
+            let input = testData.input
+
+            let output = solution.lengthOfLongestSubstring(input)
+
+            XCTAssertEqual(output, testData.expectedOutput)
+        }
+    }
+}
+
+extension LongestSubstringWithUniqueCharsOptimizedSlidingWindowSolutionTests: LongestSubstringWithUniqueCharsTestCaseProvider { }
