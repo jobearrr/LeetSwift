6ca6cbe5eed04340b171ea18225caf9ca52e42c4
Jobert Sá <bearinmind@jobear.dev>
2024-07-20 21:02:02 +0200

    ✨ Problem 004 Binary Search Solution

---
+++ Tests/SolutionsTests/004 - Median of Two Sorted Arrays/MedianOfTwoSortedArraysBinarySearchSolutionTests.swift
@@ -0,0 +1,28 @@
+//
+//  MedianOfTwoSortedArraysBinarySearchSolutionTests.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 20/07/2024.
+//
+
+import XCTest
+@testable import Problems
+@testable import Solutions
+@testable import TestSupport
+
+final class MedianOfTwoSortedArraysBinarySearchSolutionTests: XCTestCase {
+
+    let solution: MedianOfTwoSortedArraysDefinition = MedianOfTwoSortedArraysBinarySearchSolution()
+
+    func testSolution() {
+        for testData in data {
+            let input = testData.input
+
+            let output = solution.findMedianSortedArrays(input.nums1, input.nums2)
+
+            XCTAssertEqual(output, testData.expectedOutput)
+        }
+    }
+}
+
+extension MedianOfTwoSortedArraysBinarySearchSolutionTests: MedianOfTwoSortedArraysTestCaseProvider { }
