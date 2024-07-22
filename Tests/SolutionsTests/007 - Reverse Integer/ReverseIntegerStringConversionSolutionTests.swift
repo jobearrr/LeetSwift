2cc3fdce21d4208ee3c1989a695ec9b89b390071
Jobert Sá <bearinmind@jobear.dev>
2024-07-22 07:55:12 +0200

    ✨ Problem 007 Solution and Test Cases

---
+++ Tests/SolutionsTests/007 - Reverse Integer/ReverseIntegerStringConversionSolutionTests.swift
@@ -0,0 +1,28 @@
+//
+//  ReverseIntegerStringConversionSolutionTests.swift
+//  LeetSwift
+//
+//  Created by Jobert Sá on 22/07/2024.
+//
+
+import XCTest
+@testable import Problems
+@testable import Solutions
+@testable import TestSupport
+
+final class ReverseIntegerStringConversionSolutionTests: XCTestCase {
+
+    let solution: ReverseIntegerDefinition = ReverseIntegerStringConversionSolution()
+
+    func testSolution() {
+        for testData in data {
+            let input = testData.input
+
+            let output = solution.reverse(input)
+
+            XCTAssertEqual(output, testData.expectedOutput)
+        }
+    }
+}
+
+extension ReverseIntegerStringConversionSolutionTests: ReverseIntegerTestCaseProvider { }
